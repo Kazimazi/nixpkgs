7282d006a4c9d2a6dcdc963bc9aa62598cc3f893
kazimazi <attila.koszo.official@gmail.com>
2023-10-28 14:46:25 +0200

    logseq: update electron 24 -> 26

--- pkgs/applications/misc/logseq/default.nix
+++ pkgs/applications/misc/logseq/default.nix
@@ -4,21 +4,21 @@
 , appimageTools
 , makeWrapper
 # graphs will not sync without matching upstream's major electron version
-, electron_24
+, electron_26
 , git
 , nix-update-script
 }:
 
 stdenv.mkDerivation (finalAttrs: let
-  inherit (finalAttrs) pname version src appimageContents;
-
+  inherit (finalAttrs) pname tag version src appimageContents;
 in {
   pname = "logseq";
-  version = "0.9.19";
+  tag = "nightly";
+  version = "0.9.19-alpha+nightly.20231027";
 
   src = fetchurl {
-    url = "https://github.com/logseq/logseq/releases/download/${version}/logseq-linux-x64-${version}.AppImage";
-    hash = "sha256-Y3AeeJc+PYJdckpOma5ZDbVtBbjBTfNNDqTip4l02/E=";
+    url = "https://github.com/logseq/logseq/releases/download/${tag}/logseq-linux-x64-${version}.AppImage";
+    hash = "sha256-RrG3tmILvPK0y2Iat8Tf4NiKH4G2LFduCiliCf1l68g=";
     name = "${pname}-${version}.AppImage";
   };
 
@@ -57,7 +57,7 @@ in {
 
   postFixup = ''
     # set the env "LOCAL_GIT_DIRECTORY" for dugite so that we can use the git in nixpkgs
-    makeWrapper ${electron_24}/bin/electron $out/bin/${pname} \
+    makeWrapper ${electron_26}/bin/electron $out/bin/${pname} \
       --set "LOCAL_GIT_DIRECTORY" ${git} \
       --add-flags $out/share/${pname}/resources/app \
       --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}" \
@@ -69,7 +69,7 @@ in {
   meta = with lib; {
     description = "A local-first, non-linear, outliner notebook for organizing and sharing your personal knowledge base";
     homepage = "https://github.com/logseq/logseq";
-    changelog = "https://github.com/logseq/logseq/releases/tag/${version}";
+    changelog = "https://github.com/logseq/logseq/releases/tag/${tag}";
     license = licenses.agpl3Plus;
     maintainers = with maintainers; [ ];
     platforms = [ "x86_64-linux" ];
